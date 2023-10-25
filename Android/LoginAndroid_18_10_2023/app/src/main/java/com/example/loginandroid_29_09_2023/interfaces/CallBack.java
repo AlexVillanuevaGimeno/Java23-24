a1c714b818e91cacdcbbf24c208c804dfc7131ce
AlexVillanuevaGimeno <a26954@svalero.com>
2023-10-25 09:39:13 +0200

    Threads_Android_25_!0

--- Android/LoginAndroid_18_10_2023/app/src/main/java/com/example/loginandroid_29_09_2023/interfaces/CallBack.java
+++
@@ -1,7 +0,0 @@
-package com.example.loginandroid_29_09_2023.interfaces;
-
-public interface CallBack<E> {
-    void onResponse(Call<E> call, Response<E> response);
-    void onFailure(Call<E>, Throwable t);
-
-}
