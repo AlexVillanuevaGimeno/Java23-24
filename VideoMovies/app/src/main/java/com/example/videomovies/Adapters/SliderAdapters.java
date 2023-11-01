3e8f7631a34cfc3cd9cdf182f1709eaebb3699af
AlexVillanuevaGimeno <a26954@svalero.com>
2023-11-01 14:11:56 +0100

    primera parte video

---
+++ VideoMovies/app/src/main/java/com/example/videomovies/Adapters/SliderAdapters.java
@@ -0,0 +1,75 @@
+package com.example.videomovies.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.RoundedCorner;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.videomovies.Domian.SliderItems;
+import com.example.videomovies.R;
+
+import java.util.List;
+
+public class SliderAdapters extends RecyclerView.Adapter<SliderAdapters.SliderViewHolder> {
+    private List<SliderItems> sliderItems;
+    private ViewPager2 viewPager2;
+    private Context context;
+
+    public SliderAdapters(List<SliderItems> sliderItems, ViewPager2 viewPager2) {
+        this.sliderItems = sliderItems;
+        this.viewPager2 = viewPager2;
+    }
+
+    @NonNull
+    @Override
+    public SliderAdapters.SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context=parent.getContext();
+        return new SliderViewHolder(LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.slide_item_container,parent,false
+        ));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SliderAdapters.SliderViewHolder holder, int position) {
+holder.setImage(SliderItems.get(position));
+    if(position== sliderItems.size()-2){
+        viewPager2.post(runnable);
+    }
+    }
+
+    @Override
+    public int getItemCount() {
+        return sliderItems.size();
+    }
+
+
+    public class SliderViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imageView;
+        public SliderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView=itemView.findViewById(R.id.imageSlide);
+        }
+        void setImage(SliderItems sliderItems){
+            RequestOptions requestOptions =new RequestOptions();
+            requestOptions=requestOptions.transform(new CenterCrop(),new RoundedCorners(60));
+
+            Glide.with(context)
+                    .load(SliderItems.getImage())
+                    .apply(requestOptions)
+                    .into(imageView);
+        }
+    }
+    private Runnable runnable=new Runnable() {
+
+        @Override
+        public void run() {
+            sliderItems.addAll(sliderItems);
+            notifyDataSetChanged();
+        }
+    };
+}
