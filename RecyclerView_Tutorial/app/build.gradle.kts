a6cd9f198aaaa17edd88266bd0951442086fd703
AlexVillanuevaGimeno <a26954@svalero.com>
2023-11-10 13:10:23 +0100

    gradle 34

--- RecyclerView_Tutorial/app/build.gradle.kts
+++ RecyclerView_Tutorial/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.recyclerview_tutorial"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.recyclerview_tutorial"
