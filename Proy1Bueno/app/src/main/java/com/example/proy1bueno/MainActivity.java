3a46cb04a5ea2abc6e6eddba6e3103bb8f8c6d55
AlexVillanuevaGimeno <a26954@svalero.com>
2023-11-19 12:52:37 +0100

    Proy1Bueno(Android Login+Index)

--- Proy1Bueno/app/src/main/java/com/example/proy1bueno/MainActivity.java
+++ Proy1Bueno/app/src/main/java/com/example/proy1bueno/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.android1evproy;
+package com.example.proy1bueno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,20 +13,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Reemplaza "tu_layout" con el nombre de tu layout XML
 
         MaterialButton btnIndex = findViewById(R.id.btnIndex);
         btnIndex.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Código que se ejecutará al hacer clic en el botón
+                // Código que se ejecutará al hacer clic    en el botón
                 abrirLoginActivity();
             }
         });
     }
 
     private void abrirLoginActivity() {
-        Intent intent = new Intent(this, Login.class);
+        Intent intent = new Intent(this, Login.class); // Reemplaza "LoginActivity" con el nombre de tu clase de actividad de login
         startActivity(intent);
     }
 }
